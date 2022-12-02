5317832243eb83e4278d7f1e147885d1a1adbe1f
Shoeyuz <44645890+Shoeyuz@users.noreply.github.com>
2022-12-02 11:07:16 -0500

    Update server.c

--- server.c
+++ server.c
@@ -11,7 +11,7 @@
 
 #define MAX_STRING_LEN    256
 #define ATTACH_POINT "controlSystem"
-int calculate_checksum(char *text);
+int calculate_checksum(airplane *plane);
 
 int main(void) {
 
@@ -30,30 +30,27 @@ int main(void) {
 	   return EXIT_FAILURE;
    }
 
-    //the server should keep receiving, processing and replying to messages
+
 	while(1)
 	{
 	  //code to receive msg or pulse from client
 	   rcvid = MsgReceive(attach->chid, &msg, sizeof(msg), NULL);
 
-
-	  //print the returned value of MsgReceive
-	   printf("Msgreceive val: %d\n", rcvid);
-
 	   //error checking
 	   if(rcvid == -1)
 		   break;
 
-	  //check if it was a pulse
-	   if(rcvid == 0){//pulse received
+	  //pulse
+	   if(rcvid == 0){
 		   switch (msg.pulse.code) {
 
-		   // check  if the client is gone/disconnected and print (client is gone)
+		   // client is gone/disconnected
 		   	   case _PULSE_CODE_DISCONNECT:
 		                 printf("client is disconected \n");
 		                  ConnectDetach(msg.pulse.scoid);
 		                  break;
-		  //something else - printing code and val pulse
+
+		  //something else
 		   	   default:
 		   		   printf("Some other pulse -> code: %d ... val: %d\n",msg.pulse.code,msg.pulse.value.sival_int);
 
@@ -62,35 +59,35 @@ int main(void) {
 
 	   }
 
-	  // if it was a message
+	  //message
 	   else{
 		   //print received message
 	       printf("server received plane: id:%d \n", msg.plane.id);
 
-			/*calculate checksum by calling calculate_checksum
-	       int checksum = calculate_checksum(msg.rmsg);
-
-			//reply to client with checksum
+	       //carrying out data integrity verification using checksum
+	       int checksum = calculate_checksum(&msg.plane);
 	       int res = MsgReply(rcvid,0,&checksum,sizeof(checksum));
-
-			//print the return value of MsgReply
-	       printf("Msgreply val: %d\n",res);*/
+	       printf("Msgreply val: %d\n",res);
 	   }
 	}
    //remove the name from the namespace and destroy the channel
-   	   name_detach(attach,0);
-   	   return 0;
+   name_detach(attach,0);
+   return 0;
 }
 
 
 
 
-int calculate_checksum(char *text)
+int calculate_checksum(airplane *plane)
 {
-	char *c;
-	int cksum = 0;
 
-	for (c = text; *c != '\0'; c++)
-		cksum += *c;
-	return cksum;
+	unsigned int sum = 0;
+
+	unsigned char *p = (unsigned char *)plane;
+	for (int i=0; i<sizeof(plane); i++) {
+	    sum += p[i];
+	}
+
+
+	return sum;
 }
